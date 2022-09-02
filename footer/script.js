c380746b8ba48a6b04953a76da7c66a78039277e
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-09-02 23:13:27 +0530

    update script.js

--- footer/script.js
+++ footer/script.js
@@ -1,5 +1,5 @@
-let para1 =  document.getElementsByClassName("first-para")[0]
-let arr =[
+let para1 = document.getElementsByClassName("first-para")[0]
+let arr = [
     "Warburg invests $100 mn in boAt",
     "How India's boAt became the fifth largest wearable brand in the world",
     "The Airdopes 441 is sweat proof and packs in crisp audio. Great for fitness enthusiasts.",
@@ -8,11 +8,24 @@ let arr =[
 ]
 let btn = document.querySelectorAll(".logo-item")
 
-for(let i=0;i<btn.length;i++){
-   btn[i].addEventListener("mouseover",function(e){
-        if(i==0)para1.innerHTML = arr[0]
-        else if(i==1) para1.innerHTML = arr[1]
-        else if(i==2) para1.innerHTML = arr[2]
-        else if(i==3) para1.innerHTML = arr[3]
-});
+for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener("mouseover", function (e) {
+
+        switch (i) {
+            case 0:
+                para1.innerHTML = arr[0]
+                break
+
+            case 1:
+                para1.innerHTML = arr[1]
+                break
+            case 2:
+                para1.innerHTML = arr[2]
+                break
+
+            case 3:
+                para1.innerHTML = arr[3]
+                break
+        }
+    });
 }

e22c88c82d059de8c8886ccd58dcc76994a5dbb7
ketans2141 <105305997+ketans2141@users.noreply.github.com>
2022-09-12 13:03:50 +0530

    scroll to top functionality added

--- index.js
+++ index.js
@@ -3397,3 +3397,28 @@ main.addEventListener("click", (e) => {
       cart_main.innerHTML += html;
     });
 });
+
+
+// -------------------------------------------------
+
+const topArrowBtn=document.getElementById("scroll_to_top_btn");
+
+window.onscroll=function(){
+    scrollFunction()
+}
+
+function scrollFunction(){
+if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    topArrowBtn.style.display = "block";
+  } else {
+    topArrowBtn.style.display = "none";
+  }
+}
+
+topArrowBtn.addEventListener("click",function(){
+    
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      
+    
+})
